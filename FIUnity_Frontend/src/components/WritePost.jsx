3b66a09cb3e6b5e5ae481df9951030192ef9860b
PoojaLad55 <PoojaLad55@gmail.com>
2024-05-12 00:36:04 -0400

    completed homepage/visual feed; font needs to be changed

---
+++ FIUnity_Frontend/src/components/WritePost.jsx
@@ -0,0 +1,40 @@
+import "./Post.css";
+import { useState } from "react";
+import { SlPicture } from "react-icons/sl";
+import { FaRegCalendarAlt } from "react-icons/fa";
+
+export default function WritePost({ profilePic1 }) {
+  const [userInput, setUserInput] = useState("");
+
+  return (
+    <>
+      <div className="large-post-box font">
+        <img src={profilePic1} alt="profile picture" className="profile-pic" />
+        <div className="text-area-container">
+          <textarea
+            className="text-box scrollbar"
+            value={userInput}
+            onChange={(event) => {
+              setUserInput(event.target.value);
+            }}
+            placeholder="Type here..."
+          />
+          <div className="icon-button-style">
+            <div className="icon">
+              <span>
+                <SlPicture />
+                Media
+              </span>
+              <span>
+                <FaRegCalendarAlt />
+                Event
+              </span>
+            </div>
+            <button className="post-button">Submit post</button>
+          </div>
+        </div>
+      </div>
+      <div className="line"></div>
+    </>
+  );
+}
