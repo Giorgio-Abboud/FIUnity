92e074c84de7cf5be3abf9a91a3d2ae4d993e358
amendoza042004 <amendozaam04@gmail.com>
2024-05-13 12:54:30 -0400

    Job posting Form Done

---
+++ FIUnity_Frontend/src/job-posting.jsx
@@ -0,0 +1,258 @@
+import React, { useState } from 'react';
+import "./job-posting.css";
+import axios, { isCancel, AxiosError } from 'axios';
+
+
+
+const JobAddingPosting = () => {
+    const [jobPosition, setJobPosition] = useState('');
+    const [jobID, setJobID] = useState('');
+    const [companyName, setCompanyName] = useState('');
+    const [salary, setSalary] = useState('');
+    const [type, setType] = useState('');
+    const [mode, setMode] = useState('');
+    const [startDate, setStartDate] = useState('');
+    const [endDate, setEndDate] = useState('');
+    const [otherRequirements, setOtherRequirements] = useState('');
+    const [usWorkAuthorization, setUsWorkAuthorization] = useState(false);
+    const [usCitizenship, setUsCitizenship] = useState(false);
+    const [usResidency, setUsResidency] = useState(false);
+    const [applicationLink, setApplicationLink] = useState('');
+    const [isPosted, setIsPosted] = useState(false);
+    const [redirectUrl, setRedirectUrl] = useState('');
+    const [isError, setIsError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        
+        if (!jobPosition || !companyName || !salary || !type || !mode || !startDate || !usWorkAuthorization
+            || !usCitizenship || !usResidency || !applicationLink) {
+            setIsError(true);
+            setErrorMessage("Please fill out all required fields.");
+            return;
+        }
+
+        // Create an object with the job posting information
+        const jobPostingData = {
+            jobPosition,
+            jobID,
+            companyName,
+            salary,
+            type,
+            mode,
+            startDate,
+            endDate,
+            otherRequirements,
+            usWorkAuthorization,
+            usCitizenship,
+            usResidency,
+            applicationLink
+        };
+
+        // Send a POST request to the backend with the job posting information as JSON
+        axios.post("http://10.108.229.73:8000/job-posting", jobPostingData)
+            .then(response => {
+                if (response.status === 200) {
+                    console.log("Job posting successful");
+                    setIsPosted(true);
+                    setRedirectUrl('http://10.108.229.73:8000/jobs');
+                } else {
+                    console.error("Job posting failed");
+                }
+            })
+            .catch(error => {
+                console.log("There was an error");
+                console.error("Error sending job posting request:", error);
+            });
+    };
+
+
+
+    return isPosted ? <Redirect to={redirectUrl} /> : (
+        <>
+            <div>
+                <form className="job-posting" onSubmit={handleSubmit}>
+                    <h2>Post a Job to the Community</h2>
+                    <label htmlFor="jobPosition">Job Position: <span className="required">*</span></label>
+                    <input
+                        type="text"
+                        id="jobPosition"
+                        value={jobPosition}
+                        onChange={(e) => setJobPosition(e.target.value)}
+                        required
+                    />
+
+                    <label htmlFor="jobID">Job ID:</label>
+                    <input
+                        type="text"
+                        id="jobID"
+                        value={jobID}
+                        onChange={(e) => setJobID(e.target.value)}
+                    />
+
+                    <label htmlFor="companyName">Company name: <span className="required">*</span></label>
+                    <input
+                        type="text"
+                        id="companyName"
+                        value={companyName}
+                        onChange={(e) => setCompanyName(e.target.value)}
+                        required
+                    />
+
+                    <label htmlFor="salary">Salary: <span className="required">*</span></label>
+                    <input
+                        type="text"
+                        id="salary"
+                        value={salary}
+                        onChange={(e) => setSalary(e.target.value)}
+                        required
+                    />
+
+                    <label htmlFor="type">Type: <span className="required">*</span></label>
+                    <select
+                        id="type"
+                        value={type}
+                        onChange={(e) => setType(e.target.value)}
+                        required
+                    >
+                        <option value="">Choose one</option>
+                        <option value="option1">Internship</option>
+                        <option value="option2">Part-Time</option>
+                        <option value="option3">Full-Time</option>
+                    </select>
+
+                    <label htmlFor="mode">Mode: <span className="required">*</span></label>
+                    <select
+                        id="mode"
+                        value={mode}
+                        onChange={(e) => setMode(e.target.value)}
+                        required
+                    >
+                        <option value="">Choose one</option>
+                        <option value="option1">In-Person</option>
+                        <option value="option2">Hybrid</option>
+                        <option value="option3">Remote</option>
+                    </select>
+
+                    <div className="date-picker">
+                        <label htmlFor="startDate">Start Date: <span className="required">*</span></label>
+                        <input
+                            type="date"
+                            id="startDate"
+                            value={startDate}
+                            onChange={(e) => setStartDate(e.target.value)}
+                            required
+                        />
+                        <label htmlFor="endDate">End Date:</label>
+                        <input
+                            type="date"
+                            id="endDate"
+                            value={endDate}
+                            onChange={(e) => setEndDate(e.target.value)}
+                        />
+                    </div>
+
+
+                    <label htmlFor="otherRequirements">Other requirements:</label>
+                    <textarea
+                        id="otherRequirements"
+                        value={otherRequirements}
+                        placeholder="Type here..."
+                        onChange={(e) => setOtherRequirements(e.target.value)}
+                    />
+
+                    <label htmlFor="usWorkAuthorization">US Work Authorization Required: <span className="required">*</span></label>
+                    <div className="radio-container">
+                        <input
+                            type="radio"
+                            id="usWorkAuthorizationYes"
+                            name="usWorkAuthorization"
+                            value="true"
+                            checked={usWorkAuthorization === true}
+                            onChange={() => setUsWorkAuthorization(true)}
+                            required
+                        />
+                        <label htmlFor="usWorkAuthorizationYes">Yes</label>
+
+                        <input
+                            type="radio"
+                            id="usWorkAuthorizationNo"
+                            name="usWorkAuthorization"
+                            value="false"
+                            checked={usWorkAuthorization === false}
+                            onChange={() => setUsWorkAuthorization(false)}
+                            required
+                        />
+                        <label htmlFor="usWorkAuthorizationNo">No</label>
+                    </div>
+
+                    <label htmlFor="usCitizenship">US Citizenship Required: <span className="required">*</span></label>
+                    <div className="radio-container">
+                        <input
+                            type="radio"
+                            id="usCitizenshipYes"
+                            name="usCitizenship"
+                            value="true"
+                            checked={usCitizenship === true}
+                            onChange={() => setUsCitizenship(true)}
+                            required
+                        />
+                        <label htmlFor="usCitizenshipYes">Yes</label>
+
+                        <input
+                            type="radio"
+                            id="usCitizenshipNo"
+                            name="usCitizenship"
+                            value="false"
+                            checked={usCitizenship === false}
+                            onChange={() => setUsCitizenship(false)}
+                            required
+                        />
+                        <label htmlFor="usCitizenshipNo">No</label>
+                    </div>
+
+                    <label htmlFor="usResidency">US Residency Required: <span className="required">*</span></label>
+                    <div className="radio-container">
+                        <input
+                            type="radio"
+                            id="usResidencyYes"
+                            name="usResidency"
+                            value="true"
+                            checked={usResidency === true}
+                            onChange={() => setUsResidency(true)}
+                            required
+                        />
+                        <label htmlFor="usResidencyYes">Yes</label>
+
+                        <input
+                            type="radio"
+                            id="usResidencyNo"
+                            name="usResidency"
+                            value="false"
+                            checked={usResidency === false}
+                            onChange={() => setUsResidency(false)}
+                            required
+                        />
+                        <label htmlFor="usResidencyNo">No</label>
+                    </div>
+
+                    <label htmlFor="applicationLink">Application Link: <span className="required">*</span></label>
+                    <input
+                        type="text"
+                        id="applicationLink"
+                        value={applicationLink}
+                        placeholder="Type here..."
+                        onChange={(e) => setApplicationLink(e.target.value)}
+                        required
+                    />
+
+                    <button type="submit">Post</button>
+                </form>
+            </div>
+        </>
+    );
+};
+
+export default JobAddingPosting;
