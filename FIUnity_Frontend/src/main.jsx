136bd01e8be66ee87cece9a1d3c2d0eec3595bfd
derren kwee <dkwee001@fiu.edu>
2024-05-15 15:33:01 -0400

    changed color & font & background

--- FIUnity_Frontend/src/main.jsx
+++ FIUnity_Frontend/src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './regist-App.jsx'
+import './regist-index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
--- FIUnity_Frontend/src/regist-App.jsx
+++ FIUnity_Frontend/src/regist-App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./App.css";
+import "./regist-App.css";
 import FormInput from "./components/FormInput";
 
 const App = () => {
