7b585f4fb246d015bd8c78213d52e0030cdd7509
derren kwee <dkwee001@fiu.edu>
2024-05-20 15:41:39 -0400

    update something

--- FIUnity_Frontend/src/main.jsx
+++ FIUnity_Frontend/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Navigation from "./Components/NavBar/Navigation";
+import Navigation from "./components/NavBar/Navigation";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
