84471462fcd8bdf711cecaae7281985027033145
amendoza042004 <amendozaam04@gmail.com>
2024-05-20 10:44:06 -0400

    Login Page with registration button

---
+++ FIUnity_Frontend/src/Login-App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './Login-App.css'
+import RegistrationLogIn from './Log-in'
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <div>
+      <RegistrationLogIn />
+     </div>
+    </>
+  )
+}
+
+export default App
