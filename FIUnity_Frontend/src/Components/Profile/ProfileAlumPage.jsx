1e42bc5245602f30a986faad946048fe5efccb67
PoojaLad55 <PoojaLad55@gmail.com>
2024-05-30 18:04:06 -0400

    cards hard-coded

--- FIUnity_Frontend/src/Components/Profile/ProfileAlumPage.jsx
+++ FIUnity_Frontend/src/Components/Profile/ProfileAlumPage.jsx
@@ -6,8 +6,9 @@ export default function ProfileAlumPage({
   classification,
   gradYear,
   yearsOfExperience,
-  jobPosition,
+  currJobPosition,
   aboutMe,
+  jobPosition,
   projectName,
   projectDescript,
   experience,
@@ -29,10 +30,23 @@ export default function ProfileAlumPage({
                 </div>
               </div>
               <div className="profile-white-block">
-                <div className="profile-font-bold">
-                  <p className="profile-font">Grad Year:</p>
-                  <p className="profile-font">Years of Experience: </p>
-                  <p className="profile-font">Job Position:</p>
+                <div>
+                  <div className="three-text-flex">
+                    <p className="profile-font profile-font-bold">Grad Year:</p>
+                    <pm className="three-text-ans profile-font">xyz</pm>
+                  </div>
+                  <div className="three-text-flex">
+                    <p className="profile-font profile-font-bold">
+                      Years of Experience:{" "}
+                    </p>
+                    <p className="three-text-ans profile-font">xyz</p>
+                  </div>
+                  <div className="three-text-flex">
+                    <p className="profile-font profile-font-bold">
+                      Job Position:
+                    </p>
+                    <p className="three-text-ans profile-font">xyz</p>
+                  </div>
                 </div>
               </div>
               <div className="about-us-container">
@@ -52,13 +66,32 @@ export default function ProfileAlumPage({
             <div className="experience-container">
               <h3 className="title-text profile-font">EXPERIENCE</h3>
               <div className="title-experience-line"></div>
+              <div className="profile-font-bold">
+                <p className="profile-title profile-font profile-font-bold">
+                  Job Position
+                </p>
+                <p className="profile-font experience-text profile-font-bold">
+                  Location
+                </p>
+                <p className="profile-font experience-text profile-font-bold">
+                  Time
+                </p>
+              </div>
+              <p className="profile-descript profile-font">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
+              </p>
             </div>
           </div>
           <div className="project-container">
             <h3 className="title-text profile-font">PROJECTS</h3>
             <div className="title-project-line"></div>
             <div>
-              <h4 className="project-title profile-font">Project 1</h4>
+              <h4 className="profile-title profile-font-bold profile-font">
+                Project 1
+              </h4>
               <p className="profile-descript profile-font">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
                 eiusmod tempor incididunt ut labore et dolore magna aliqua.{" "}
