6411fa8ef2ce619d55b641fe305cde28e9bbfa48
amendoza042004 <amendozaam04@gmail.com>
2024-05-31 15:00:13 -0500

    Alumni and Student Edit profile page modified.

--- FIUnity_Frontend/src/Components/ProfileEdit/profileEdit.jsx
+++ FIUnity_Frontend/src/Components/ProfileEdit/profileEdit.jsx
@@ -7,14 +7,27 @@ const ProfileEdit = () => {
     const [profile, setProfile] = useState({
         firstName: '',
         lastName: '',
+        middleName: '',
         graduationYear: '',
+        gradTerm: '',
         classStanding: '',
         major: '',
         aboutMe: '',
         profilePicture: null
     });
-    const [experiences, setExperiences] = useState([{ jobPosition: '', companyName: '', startDate: '', endDate: '', description: '' }]);
+    const [experiences, setExperiences] = useState([{
+        jobPosition: '',
+        companyName: '',
+        type: '',
+        location: '',
+        startDate: '',
+        endDate: '',
+        current: '',
+        description: ''
+    }]);
     const [projects, setProjects] = useState([{ projectName: '', description: '' }]);
+    const [skills, setSkills] = useState([{ skillName: '', proficiency: '' }]);
+
 
     const handleProfileChange = (e) => {
         const { name, value } = e.target;
@@ -35,17 +48,37 @@ const ProfileEdit = () => {
         setProjects(newProjects);
     };
 
+    const handleSkillChange = (index, e) => {
+        const { name, value } = e.target;
+        const newSkills = skills.slice();
+        newSkills[index][name] = value;
+        setSkills(newSkills);
+    };
+
     const handleProfilePictureChange = (e) => {
         // Update the profile picture state when a new file is selected
         setProfile({ ...profile, profilePicture: e.target.files[0] });
     };
 
     const addExperience = () => {
-        setExperiences([...experiences, { jobPosition: '', companyName: '', startDate: '', endDate: '', description: '' }]);
+        setExperiences([...experiences, {
+            jobPosition: '',
+            companyName: '',
+            type: '',
+            location: '',
+            startDate: '',
+            endDate: '',
+            current: '',
+            description: ''
+        }]);
     };
 
     const addProject = () => {
         setProjects([...projects, { projectName: '', description: '' }]);
+    };
+
+    const addSkill = () => {
+        setSkills([...skills, { skillName: '', proficiency: '' }]);
     };
 
     const handleSubmit = async (e) => {
@@ -57,7 +90,8 @@ const ProfileEdit = () => {
         const profileData = {
             profile,
             experiences,
-            projects
+            projects,
+            skills
         };
 
         console.log('It got here too')
@@ -107,6 +141,15 @@ const ProfileEdit = () => {
                             onChange={handleProfileChange}
                             required
                         />
+                        <label htmlFor="middleName">Middle Name </label>
+                        <input
+                            type="text"
+                            id="middleName"
+                            name="middleName"
+                            value={profile.middleName}
+                            onChange={handleProfileChange}
+
+                        />
                         <label htmlFor="lastName">Last Name <div className="required-fields">*</div></label>
                         <input
                             type="text"
@@ -119,6 +162,15 @@ const ProfileEdit = () => {
                     </div>
                 </div>
 
+                <label htmlFor="aboutMe">About Me</label>
+                <textarea
+                    id="aboutMe"
+                    placeholder = "You can write about your years of experience, industry, or skills. People also talk about their achievements or previous job experiences."
+                    name="aboutMe"
+                    value={profile.aboutMe}
+                    onChange={handleProfileChange}
+                />
+
                 <label htmlFor="graduationYear">Graduation Year <div className="required-fields">*</div></label>
                 <input
                     type="text"
@@ -128,6 +180,20 @@ const ProfileEdit = () => {
                     onChange={handleProfileChange}
                     required
                 />
+
+                <label htmlFor="gradTerm">Graduation Term <div className="required-fields">*</div> </label>
+                <select
+                    id="gradTerm"
+                    value={profile.gradTerm}
+                    onChange={(e) => setType(e.target.value)}
+                    required
+                >
+                    <option value="">Choose one</option>
+                    <option value="option1">Spring</option>
+                    <option value="option2">Summer</option>
+                    <option value="option3">Fall</option>
+                </select>
+
 
                 <label htmlFor="classStanding">Class Standing <div className="required-fields">*</div> </label>
                 <select
@@ -153,14 +219,6 @@ const ProfileEdit = () => {
                     required
                 />
 
-                <label htmlFor="aboutMe">About Me</label>
-                <textarea
-                    id="aboutMe"
-                    name="aboutMe"
-                    value={profile.aboutMe}
-                    onChange={handleProfileChange}
-                />
-
                 <h3>Experiences</h3>
                 {experiences.map((experience, index) => (
                     <div key={index} className="experience-section">
@@ -184,6 +242,33 @@ const ProfileEdit = () => {
                             required
                         />
 
+                        <label htmlFor={`type-${index}`}>Type</label>
+                        <select
+                            id={`type-${index}`}
+                            name="type"
+                            value={experience.type}
+                            onChange={(e) => handleExperienceChange(index, e)}
+                        >
+                            <option value="">Choose One</option>
+                            <option value="Full-time">Full-time</option>
+                            <option value="Part-time">Part-time</option>
+                            <option value="Self-employed">Self-employed</option>
+                            <option value="Freelance">Freelance</option>
+                            <option value="Contract">Contract</option>
+                            <option value="Internship">Internship</option>
+                            <option value="Apprenticeship">Apprenticeship</option>
+                            <option value="Seasonal">Seasonal</option>
+                        </select>
+
+                        <label htmlFor={`location-${index}`}>Location</label>
+                        <input
+                            type="text"
+                            id={`location-${index}`}
+                            name="location"
+                            value={experience.location}
+                            onChange={(e) => handleExperienceChange(index, e)}
+                        />
+
                         <label htmlFor={`startDate-${index}`}>Start Date <div className="required-fields">*</div></label>
                         <input
                             type="date"
@@ -202,6 +287,18 @@ const ProfileEdit = () => {
                             value={experience.endDate}
                             onChange={(e) => handleExperienceChange(index, e)}
                         />
+
+                        <div className="experience-checkbox">
+
+                            <input
+                                type="checkbox"
+                                name="current"
+                                checked={experience.current}
+                                onChange={(e) => handleExperienceChange(index, e)}
+                            />
+                            <label className="checkbox-label"> I'm currently in this position</label>
+
+                        </div>
 
                         <label htmlFor={`description-${index}`}>Description </label>
                         <textarea
@@ -237,6 +334,36 @@ const ProfileEdit = () => {
                     </div>
                 ))}
                 <button type="button" onClick={addProject}>Add More</button>
+
+                <h3>Skills</h3>
+                {skills.map((skill, index) => (
+                    <div key={index} className="skills-section">
+                        <label htmlFor={`skillName-${index}`}>Skill Name <div className="required-fields">*</div></label>
+                        <input
+                            type="text"
+                            id={`skillName-${index}`}
+                            name="skillName"
+                            value={skill.skillName}
+                            onChange={(e) => handleSkillChange(index, e)}
+                            required
+                        />
+
+                        <label htmlFor={`proficiency-${index}`}>Proficiency</label>
+                        <select
+                            id={`proficiency-${index}`}
+                            name="proficiency"
+                            value={skill.proficiency}
+                            onChange={(e) => handleSkillChange(index, e)}
+                        >
+                            <option value="">Choose One</option>
+                            <option value="Beginner">Beginner</option>
+                            <option value="Intermediate">Intermediate</option>
+                            <option value="Advanced">Advanced</option>
+                            <option value="Expert">Expert</option>
+                        </select>
+                    </div>
+                ))}
+                <button type="button" onClick={addSkill}>Add More</button>
 
                 <button type="submit">Save</button>
             </form>
--- FIUnity_Frontend/src/Components/ProfileEdit/profileEditAlumni.jsx
+++ FIUnity_Frontend/src/Components/ProfileEdit/profileEditAlumni.jsx
@@ -7,14 +7,25 @@ const ProfileEditAlumni = () => {
     const [profile, setProfile] = useState({
         firstName: '',
         lastName: '',
+        middleName: '',
         graduationYear: '',
-        yearsExperience: '',
+        gradTerm: '',
         current_job_position: '',
         aboutMe: '',
         profilePicture: null
     });
-    const [experiences, setExperiences] = useState([{ jobPosition: '', companyName: '', startDate: '', endDate: '', description: '' }]);
-    const [projects, setProjects] = useState([{ projectName: '', description: '' }]);
+
+    const [experiences, setExperiences] = useState([{ 
+        jobPosition: '', 
+        companyName: '', 
+        type: '', 
+        location: '', 
+        startDate: '', 
+        endDate: '', 
+        current:'', 
+        description: '' }]);
+
+    const [skills, setSkills] = useState([{ skillName: '', proficiency: '' }]);
 
     const handleProfileChange = (e) => {
         const { name, value } = e.target;
@@ -22,17 +33,22 @@ const ProfileEditAlumni = () => {
     };
 
     const handleExperienceChange = (index, e) => {
-        const { name, value } = e.target;
+        const { name, value, type, checked } = e.target;
         const newExperiences = experiences.slice();
-        newExperiences[index][name] = value;
+        if (type === "checkbox") {
+            newExperiences[index][name] = checked;
+        } else {
+            newExperiences[index][name] = value;
+        }
         setExperiences(newExperiences);
     };
 
-    const handleProjectChange = (index, e) => {
+
+    const handleSkillChange = (index, e) => {
         const { name, value } = e.target;
-        const newProjects = projects.slice();
-        newProjects[index][name] = value;
-        setProjects(newProjects);
+        const newSkills = skills.slice();
+        newSkills[index][name] = value;
+        setSkills(newSkills);
     };
 
     const handleProfilePictureChange = (e) => {
@@ -41,11 +57,19 @@ const ProfileEditAlumni = () => {
     };
 
     const addExperience = () => {
-        setExperiences([...experiences, { jobPosition: '', companyName: '', startDate: '', endDate: '', description: '' }]);
+        setExperiences([...experiences, { 
+        jobPosition: '', 
+        companyName: '', 
+        type: '', 
+        location: '', 
+        startDate: '', 
+        endDate: '', 
+        current:'', 
+        description: '' }]);
     };
 
-    const addProject = () => {
-        setProjects([...projects, { projectName: '', description: '' }]);
+    const addSkill = () => {
+        setSkills([...skills, { skillName: '', proficiency: '' }]);
     };
 
     const handleSubmit = async (e) => {
@@ -57,12 +81,12 @@ const ProfileEditAlumni = () => {
         const profileData = {
             profile,
             experiences,
-            projects
+            skills
         };
 
         console.log('It got here too')
         try {
-            const response = await axios.post("http://localhost:8008/profile/profile-edit/", profileData);
+            const response = await axios.post("http://localhost:8008/profile/profile-edit-alumni/", profileData);
             if (response.status === 201) {
                 console.log("Profile edit successful");
             } else {
@@ -107,6 +131,15 @@ const ProfileEditAlumni = () => {
                             onChange={handleProfileChange}
                             required
                         />
+                        <label htmlFor="middleName">Middle Name </label>
+                        <input
+                            type="text"
+                            id="middleName"
+                            name="middleName"
+                            value={profile.middleName}
+                            onChange={handleProfileChange}
+                            
+                        />
                         <label htmlFor="lastName">Last Name <div className="required-fields">*</div></label>
                         <input
                             type="text"
@@ -116,6 +149,7 @@ const ProfileEditAlumni = () => {
                             onChange={handleProfileChange}
                             required
                         />
+
                     </div>
                 </div>
 
@@ -129,29 +163,23 @@ const ProfileEditAlumni = () => {
                     required
                 />
 
-                <label htmlFor="yearsExperience">Years of Experience <div className="required-fields">*</div></label>
-                <input
-                    type="text"
-                    id="yearsExperience"
-                    name="yearsExperience"
-                    value={profile.yearsExperience}
-                    onChange={handleProfileChange}
+                <label htmlFor="gradTerm">Graduation Term <div className="required-fields">*</div> </label>
+                <select
+                    id="gradTerm"
+                    value={profile.gradTerm}
+                    onChange={(e) => setType(e.target.value)}
                     required
-                />
-
-                <label htmlFor="current_job_position">Current Job Position <div className="required-fields">*</div></label>
-                <input
-                    type="text"
-                    id="current_job_position"
-                    name="current_job_position"
-                    value={profile.current_job_position}
-                    onChange={handleProfileChange}
-                    required
-                />
+                >
+                    <option value="">Choose one</option>
+                    <option value="option1">Spring</option>
+                    <option value="option2">Summer</option>
+                    <option value="option3">Fall</option>
+                </select>
 
                 <label htmlFor="aboutMe">About Me</label>
                 <textarea
                     id="aboutMe"
+                    placeholder = "You can write about your years of experience, industry, or skills. People also talk about their achievements or previous job experiences."
                     name="aboutMe"
                     value={profile.aboutMe}
                     onChange={handleProfileChange}
@@ -180,6 +208,33 @@ const ProfileEditAlumni = () => {
                             required
                         />
 
+                        <label htmlFor={`type-${index}`}>Type</label>
+                        <select
+                            id={`type-${index}`}
+                            name="type"
+                            value={experience.type}
+                            onChange={(e) => handleExperienceChange(index, e)}
+                        >
+                            <option value="">Choose One</option>
+                            <option value="Full-time">Full-time</option>
+                            <option value="Part-time">Part-time</option>
+                            <option value="Self-employed">Self-employed</option>
+                            <option value="Freelance">Freelance</option>
+                            <option value="Contract">Contract</option>
+                            <option value="Internship">Internship</option>
+                            <option value="Apprenticeship">Apprenticeship</option>
+                            <option value="Seasonal">Seasonal</option>
+                        </select>
+
+                        <label htmlFor={`location-${index}`}>Location</label>
+                        <input
+                            type="text"
+                            id={`location-${index}`}
+                            name="location"
+                            value={experience.location}
+                            onChange={(e) => handleExperienceChange(index, e)}
+                        />
+
                         <label htmlFor={`startDate-${index}`}>Start Date <div className="required-fields">*</div></label>
                         <input
                             type="date"
@@ -199,6 +254,18 @@ const ProfileEditAlumni = () => {
                             onChange={(e) => handleExperienceChange(index, e)}
                         />
 
+                        <div className="experience-checkbox">
+
+                            <input
+                                type="checkbox"
+                                name="current"
+                                checked={experience.current}
+                                onChange={(e) => handleExperienceChange(index, e)}
+                            />
+                            <label className="checkbox-label"> I'm currently in this position</label>
+
+                        </div>
+
                         <label htmlFor={`description-${index}`}>Description </label>
                         <textarea
                             id={`description-${index}`}
@@ -210,29 +277,35 @@ const ProfileEditAlumni = () => {
                 ))}
                 <button type="button" onClick={addExperience}>Add More</button>
 
-                <h3>Projects</h3>
-                {projects.map((project, index) => (
-                    <div key={index} className="project-section">
-                        <label htmlFor={`projectName-${index}`}>Project Name <div className="required-fields">*</div></label>
+                <h3>Skills</h3>
+                {skills.map((skill, index) => (
+                    <div key={index} className="skills-section">
+                        <label htmlFor={`skillName-${index}`}>Skill Name <div className="required-fields">*</div></label>
                         <input
                             type="text"
-                            id={`projectName-${index}`}
-                            name="projectName"
-                            value={project.projectName}
-                            onChange={(e) => handleProjectChange(index, e)}
+                            id={`skillName-${index}`}
+                            name="skillName"
+                            value={skill.skillName}
+                            onChange={(e) => handleSkillChange(index, e)}
                             required
                         />
 
-                        <label htmlFor={`description-${index}`}>Description </label>
-                        <textarea
-                            id={`description-${index}`}
-                            name="description"
-                            value={project.description}
-                            onChange={(e) => handleProjectChange(index, e)}
-                        />
+                        <label htmlFor={`proficiency-${index}`}>Proficiency</label>
+                        <select
+                            id={`proficiency-${index}`}
+                            name="proficiency"
+                            value={skill.proficiency}
+                            onChange={(e) => handleSkillChange(index, e)}
+                        >
+                            <option value="">Choose One</option>
+                            <option value="Beginner">Beginner</option>
+                            <option value="Intermediate">Intermediate</option>
+                            <option value="Advanced">Advanced</option>
+                            <option value="Expert">Expert</option>
+                        </select>
                     </div>
                 ))}
-                <button type="button" onClick={addProject}>Add More</button>
+                <button type="button" onClick={addSkill}>Add More</button>
 
                 <button type="submit">Save</button>
             </form>
