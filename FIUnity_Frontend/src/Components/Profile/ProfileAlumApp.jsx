d3a912bab1d6eb37d34ca6255704867f8d395bfd
PoojaLad55 <PoojaLad55@gmail.com>
2024-05-31 16:05:41 -0400

    did conditional rendering of alumni and student profiles to display differently according to classification

--- FIUnity_Frontend/src/Components/Profile/ProfileAlumApp.jsx
+++ FIUnity_Frontend/src/Components/Profile/ProfileAlumApp.jsx
@@ -6,6 +6,7 @@ const exampleExperiences = [
     jobTitle: "Software Engineer",
     location: "San Francisco, CA",
     timePeriod: "Jan 2020 - Present",
+    jobType: "Part-time",
     description:
       "Worked on developing web applications using React and Node.js.",
   },
@@ -13,8 +14,9 @@ const exampleExperiences = [
     jobTitle: "Junior Developer",
     location: "New York, NY",
     timePeriod: "Jun 2018 - Dec 2019",
+    jobType: "Internship",
     description:
-      "Assisted in developing mobile applications and performing code reviews.",
+      "Assisted in developing mobile applications and performing code reviews.", //discuss how size increases and boxes look weird
   },
 ];
 
@@ -26,9 +28,32 @@ const exampleProjects = [
   {
     name: "Project 2",
     description:
-      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
   },
 ];
+
+const extracurriculars = [
+  {
+    name: "Women in Computer Science",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+  },
+  {
+    name: "INIT",
+    description:
+      "Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. ",
+  },
+];
+
+
+const skills = [
+  {
+    name: "Java",
+  },
+  {
+    name: "Django",
+  },
+];
+
 
 function ProfileAlumApp() {
   return (
@@ -36,14 +61,17 @@ function ProfileAlumApp() {
       <ProfileAlumPage
         firstName="Roary"
         lastName="Royce"
-        classification="Alumni"
-        gradYear="2020" //set four digit limit
-        yearsOfExperience="4" //set two digit limit
+        classification="Student"
+        gradDate="Fall 2020" //calander?
+        major="Computer Science"
         currJobPosition="Senior Developer" //set four word limit
+        careerInterest="Full-Stack Development"
         aboutMe="Lorem ipsum dolor sit amet, consectetu" //max char count: 850
         profilePic="/images/roary-profile-pic.jpg"
         experiences={exampleExperiences}
         projects={exampleProjects}
+        skills={skills}
+        extracurriculars={extracurriculars}
       /> 
     </>
   );
--- FIUnity_Frontend/src/Components/Profile/ProfileAlumPage.jsx
+++ FIUnity_Frontend/src/Components/Profile/ProfileAlumPage.jsx
@@ -4,22 +4,29 @@ export default function ProfileAlumPage({
   firstName,
   lastName,
   classification,
-  gradYear,
-  yearsOfExperience,
+  gradDate,
   currJobPosition,
+  careerInterest,
+  major,
   aboutMe,
   projects,
   experiences,
   profilePic,
+  skills,
+  extracurriculars,
 }) {
   return (
     <>
-      <div className="profile-linear-gradient">
+      <div className="profile-box">
         <div className="profile-large-container">
           <div className="profile-flex">
             <div>
               <div className="profile-white-block">
-                <img src={profilePic} alt="profile picture" className="profile-p-pic" />
+                <img
+                  src={profilePic}
+                  alt="profile picture"
+                  className="profile-p-pic"
+                />
                 <div className="profile-size">
                   <h1 className="profile-font profile-name-size">
                     {firstName} {lastName}
@@ -32,24 +39,37 @@ export default function ProfileAlumPage({
               <div className="profile-white-block">
                 <div>
                   <div className="three-text-flex">
-                    <p className="profile-font profile-font-bold">Grad Year:</p>
-                    <p className="three-text-ans profile-font">{gradYear}</p>
+                    <p className="profile-font profile-font-bold">Grad Date:</p>
+                    <p className="three-text-ans profile-font">{gradDate}</p>
                   </div>
-                  <div className="three-text-flex">
-                    <p className="profile-font profile-font-bold">
-                      Years of Experience:{" "}
-                    </p>
-                    <p className="three-text-ans profile-font">
-                      {yearsOfExperience}
-                    </p>
-                  </div>
-                  <div className="three-text-flex">
-                    <p className="profile-font profile-font-bold">
-                      Job Position:
-                    </p>
-                    <p className="three-text-ans profile-font">
-                      {currJobPosition}
-                    </p>
+                  <div className="three-text-">
+                    {classification == "Alumni" ? (
+                      <div className="three-text-flex">
+                        <p className="profile-font profile-font-bold">
+                          Job Position:
+                        </p>
+                        <p className="three-text-ans profile-font">
+                          {currJobPosition}
+                        </p>
+                      </div>
+                    ) : (
+                      <>
+                        <div className="three-text-flex">
+                          <p className="profile-font profile-font-bold">
+                            Major:
+                          </p>
+                          <p className="three-text-ans profile-font">{major}</p>
+                        </div>
+                        <div className="three-text-flex">
+                          <p className="profile-font profile-font-bold">
+                            Career Interest:
+                          </p>
+                          <p className="three-text-ans profile-font">
+                            {careerInterest}
+                          </p>
+                        </div>
+                      </>
+                    )}
                   </div>
                 </div>
               </div>
@@ -64,41 +84,95 @@ export default function ProfileAlumPage({
             <div className="experience-container">
               <h3 className="title-text profile-font">EXPERIENCE</h3>
               <div className="title-experience-line"></div>
-              {experiences.map((experience, index) => (
-                <div key={index}>
-                  <div>
-                    <p className="profile-title profile-font profile-font-bold">
-                      {experience.jobTitle}
-                    </p>
-                    <p className="profile-font experience-text profile-gold">
-                      {experience.location}
-                    </p>
-                    <p className="profile-font experience-text profile-gold experience-time">
-                      {experience.timePeriod}
-                    </p>
-                  </div>
-                  <p className="profile-descript profile-font">
-                    {experience.description}
-                  </p>
+              {experiences.length > 0 && (
+                <div>
+                  {experiences.map((experience, index) => (
+                    <div key={index}>
+                      <div className="experience-tab-container">
+                        <div>
+                          <p className="profile-title profile-font profile-font-bold">
+                            {experience.jobTitle}
+                          </p>
+                          <p className="profile-font experience-text profile-gold">
+                            {experience.location}
+                          </p>
+                          <p className="profile-font experience-text profile-gold experience-time-type">
+                            {experience.timePeriod}
+                          </p>
+                          <p className="profile-font experience-text profile-gold experience-time-type">
+                            {experience.jobType}
+                          </p>
+                        </div>
+                        <p className="profile-descript profile-font">
+                          {experience.description}
+                        </p>
+                      </div>
+                    </div>
+                  ))}
                 </div>
-              ))}
+              )}
             </div>
           </div>
-          <div className="project-container">
-            <h3 className="title-text profile-font">PROJECTS</h3>
-            <div className="title-project-line"></div>
-            {projects.map((project, index) => (
-              <div key={index}>
-                <div>
-                  <h4 className="profile-title profile-font-bold profile-font">
-                    {project.name}
-                  </h4>
-                  <p className="profile-descript profile-font">
-                    {project.description}
-                  </p>
-                </div>
+          {classification == "Student" && (
+            <>
+              <div className="project-extra-container">
+                <h3 className="title-text profile-font">PROJECTS</h3>
+                <div className="title-project-line"></div>
+                {projects.length > 0 && (
+                  <div>
+                    {projects.map((project, index) => (
+                      <div key={index}>
+                        <div className="skills-project-tab-container">
+                          <h4 className="profile-title profile-font-bold profile-font">
+                            {project.name}
+                          </h4>
+                          <p className="profile-descript profile-font">
+                            {project.description}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                )}
               </div>
-            ))}
+              <div className="project-extra-container">
+                <h3 className="title-text profile-font">EXTRACURRICULARS</h3>
+                <div className="title-extra-line"></div>
+                {extracurriculars.length > 0 && (
+                  <div>
+                    {extracurriculars.map((extracurricular, index) => (
+                      <div key={index}>
+                        <div className="skills-project-tab-container">
+                          <h4 className="profile-title profile-font-bold profile-font">
+                            {extracurricular.name}
+                          </h4>
+                          <p className="profile-descript profile-font">
+                            {extracurricular.description}
+                          </p>
+                        </div>
+                      </div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            </>
+          )}
+          <div className="skills-container">
+            <h3 className="title-text profile-font">SKILLS</h3>
+            <div className="title-skills-line"></div>
+            {skills.length > 0 && (
+              <div>
+                {skills.map((skill, index) => (
+                  <div key={index}>
+                    <div className="skills-project-tab-container">
+                      <p className="profile-skills profile-gold profile-font-bold">
+                        {skill.name}
+                      </p>
+                    </div>
+                  </div>
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
